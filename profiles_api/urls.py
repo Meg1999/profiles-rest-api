da74a4ca82aa6c304a58b481fc1ca3155e6d438d
Meg1999 <megha.maltesh.1999@gmail.com>
2023-03-23 15:40:12 +0530

    Added user profile feed API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profiles', views.UserProfileViewset)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 
 from profiles_api import serializers
@@ -116,6 +117,7 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle deleting an object by its id"""
         return Response({'http_method': 'DELETE'})
     
+    
 class UserProfileViewset(viewsets.ModelViewSet):
     """Handles creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
@@ -125,10 +127,23 @@ class UserProfileViewset(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
+
 class UserLoginAPIView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication, )
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user. 
+        Overrides creating an object through the model viewset """
+        serializer.save(user_profile=self.request.user)
+
 
 
 
